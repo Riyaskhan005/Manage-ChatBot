1b82390a8d4d3aa224ae66a2bffe4131a8e3f20b
Riyas khan <m.driyaskhan55@gmail.com>
2026-01-26 10:40:05 +0530

    confiq file updated

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SQLALCHEMY_ECHO = False  
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:Riyas123@localhost/nocodechatbot_db')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://Aalimas:Riyas123@Aalimas.mysql.pythonanywhere-services.com/Aalimas$aalimas_db')
